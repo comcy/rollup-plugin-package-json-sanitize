5ee54fc61fdddbe2778a5ac5d66638f757b6056d
comcy <christian.silfang@gmail.com>
2023-11-30 22:13:18 +0100

    Adde cli class

---
+++ src/cli.ts
@@ -0,0 +1,22 @@
+/**
+ * @copyright Copyright (c) 2023 Christian Silfang
+ */
+
+export interface CliParameter {
+  op: Operation;
+  props: Property[];
+}
+
+const OPERATION = {
+  remove: "Remove",
+} as const;
+
+const PROPERTY = {
+  scripts: "scripts",
+  devDependencies: "devDependencies",
+} as const;
+
+type Property = keyof typeof PROPERTY;
+type Operation = keyof typeof OPERATION;
+
+export { Operation, Property };
