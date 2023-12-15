5402bcdf2a805e525fe9a60240639780d728013e
Christian Silfang <christian.silfang@gmail.com>
2023-12-15 23:12:16 +0100

    Upated file name parameters

--- src/index.ts
+++ src/index.ts
@@ -8,28 +8,31 @@ import { CliParameter } from "./cli";
 
 export default function cleanPackageJSON(parameters: CliParameter): Plugin {
   if (!parameters) {
-    console.log("No parameters given");
+    // console.log("No parameters given");
     throw new Error("No parameters given");
   }
-  console.log("Parameters: ", parameters);
+  // console.log("Parameters: ", parameters);
 
   return {
     name: "cleanPackageJson",
 
     generateBundle(_: any, bundle: OutputBundle) {
-      const targetDirectory = parameters.targetDirectory; // Pfad zur package.json-Datei
-      const srcPackageJSONPath = parameters.sourceFilePath; // Pfad zur package.json-Datei
-      const targetPackageJSONPath = parameters.targetFileName; // Pfad zur package.json-Datei
+      const srcPackageJSONPath = parameters.sourceFilePath; 
+      const targetDirectory = parameters.targetDirectory;    
+      const targetPackageJSONPath = parameters.targetFileName; 
       const packageJSON = JSON.parse(readFileSync(srcPackageJSONPath, "utf-8"));
 
-      console.log("packageJSON >>> : ", packageJSON);
+      // console.log("packageJSON >>> : ", packageJSON);
 
       if (parameters.config.length > 0) {
         parameters.config.forEach((config) => {
+          console.log('CONFIG >>> : ', config);
           switch (config.operation) {
             case "remove":
               console.log(" --------- DELETE -------- ");
+              console.log(" property to remove: ", config.propertyName);
               delete packageJSON[config.propertyName];
+              writePackageJSON(packageJSON, targetDirectory, targetPackageJSONPath);
               break;
             case "add":
               packageJSON[config.propertyName] = config.value;
@@ -44,17 +47,39 @@ export default function cleanPackageJSON(parameters: CliParameter): Plugin {
       //   delete packageJSON.scripts; // Entferne die gesamte "scripts"-Sektion
       // }
 
-      // Schreibe die aktualisierte package.json zurück
-      console.log("packageJSON >>> : ", packageJSON);
+      // console.log("packageJSON >>> : ", packageJSON);
 
-      if (!existsSync(targetPackageJSONPath)) {
-        mkdirSync(targetDirectory);
+      // if (!existsSync(targetPackageJSONPath)) {
+      //   mkdirSync(targetDirectory);
 
-        writeFileSync(
-          targetPackageJSONPath,
-          JSON.stringify(packageJSON, null, 2)
-        );
-      }
+      //   writeFileSync(
+      //     `${targetDirectory}/${targetPackageJSONPath}`,
+      //     JSON.stringify(packageJSON, null, 2)
+      //   );
+      // }
     },
   };
+}
+
+function writePackageJSON(packageJSON: any, targetDirectoryPath: string, targetPackageJSONPath: string) {
+
+  console.log('PACKAGE JSON >>> : ', packageJSON);
+  const filePath = `${targetDirectoryPath}/${targetPackageJSONPath}`
+
+  console.log('FILEPATH >>> : ', filePath);
+
+  if (!existsSync(targetDirectoryPath)) {
+    mkdirSync(targetDirectoryPath);
+
+    writeFileSync(
+      filePath,
+      JSON.stringify(packageJSON, null, 2)
+    );
+  } else {
+    writeFileSync(
+      filePath,
+      JSON.stringify(packageJSON, null, 2)
+    );
+  }
+  // writeFileSync(filePath, JSON.stringify(packageJSON, null, 2));
 }
